49105f2c1e6eb5fac29ad318532679cf9431ef8d
I am Groot <144456703+i4mGr0ot@users.noreply.github.com>
2024-07-11 19:14:17 +0530

    Update stichingmain.py

--- stichingmain.py
+++ stichingmain.py
@@ -2,4 +2,20 @@ import numpy as np
 import cv2
 import glob
 import imutils 
-image_paths = 
+
+img_path = glob.glob("") #insert file path 
+img = [] 
+
+for i in img_path:
+  j = cv2.imread(i)
+  img.append(j)
+  cv2.imshow("Image",j)
+  cv2.waitKey(0)
+
+sticher = cv2.Sticher_create()
+error, stcImg = Sticher.stich(img)
+
+if not error:
+  cv2.imwrite("stcOutput.png", stcImg)
+  cv2.imshow("Stiched Image", stcImg)
+  cv2.waitKey(0)
